8ff616d5e7a1e1244275a5de80fe3a4f5beb8f6c
zahangir <alamzahangir218@gmail.com>
2024-02-01 23:17:03 +0600

    at last footer is added now

---
+++ client/src/components/Home/FlashSale.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import sale from '../../assets/sale.gif'
+const FlashSale = () => {
+    return (
+        <div className='container w-[95%] mx-auto my-5'>
+            <img className='w-full' src={sale} alt="" />
+        </div>
+    );
+};
+
+export default FlashSale;
